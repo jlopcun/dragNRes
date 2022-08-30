0335dc9866ca013d5ff008b8e162af1a3865ee70
jlopcun <62543437+jlopcun@users.noreply.github.com>
2022-08-30 16:39:45 +0200

    commit

---
+++ dragNRes.js
@@ -0,0 +1,25 @@
+
+//NOTE: you have to sytle the element with {position:absolute;top:0;left:0;resize:both;overflow:auto;}
+
+
+const dragNRes = (el) =>{
+    el.addEventListener('mousedown',(e)=>{
+        //this condicional is to avoid dragging element when resizing the element
+        if(!(e.clientX - Number(el.style.left.slice(0,-2)) <el.clientWidth - 20) && !(e.clientY - Number(el.style.top.slice(0,-2)) < el.clientHeight - 20)) return
+        el.addEventListener('mousemove',function drag(e){
+            document.getElementById('pepe').textContent = `${el.clientWidth} ${el.clientHeight}`
+            el.style.top = `${e.clientY - el.clientHeight/2}px`
+            el.style.left = `${e.clientX - el.clientWidth/2}px`
+            el.addEventListener('mouseup',()=>el.removeEventListener('mousemove',drag))
+        })
+    })
+
+    el.addEventListener('touchmove',function drag(e){
+            //this condicional is to avoid dragging element when resizing the element
+            if(!(e.touches[0].clientX - Number(el.style.left.slice(0,-2)) <el.clientWidth - 20) && !(e.touches[0].clientY - Number(el.style.top.slice(0,-2)) < el.clientHeight - 20)) return
+            document.getElementById('pepe').textContent = `${el.clientWidth} ${el.clientHeight}`
+            el.style.top = `${e.touches[0].clientY - el.clientHeight/2}px`
+            el.style.left = `${e.touches[0].clientX - el.clientWidth/2}px`
+            
+    })
+}
